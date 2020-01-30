72a0fd105f89b8cdeeeb99c674fc6b01528524b3
Alvaro Armas <aarmas92@outlook.com>
2020-01-30 18:34:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,1 @@
 # Write your code here.
-katz_deli = []
-
-def line (katz_deli)
-  pos = 0
-  katzDeliLine = [] 
-  if katzline.length == 0 
-    puts "The line is currently empty"
-  else
-    katzline.each do |customer|
-    katzDeliLine.push("#{pos}. #{customer}")
-    pos += 1
-    end
-    puts "The line is currently: #{katzDeliLine.join("")} "
-  end
-end
-
