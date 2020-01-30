66953e3448b6ca6ebe1646c7380b3da63856bb19
Alvaro Armas <aarmas92@outlook.com>
2020-01-30 18:09:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 def line (name) 
+  name = |"Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"|
   if puts "The line is currently empty."
   else
-    return "The line currently"
+    return "The line currently #{name}"
 end
 
